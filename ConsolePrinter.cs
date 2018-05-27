ad74f052a1583576856f5c2b326193d8efd2fc7e
NicDeSilva <nic.desilva99@gmail.com>
2018-05-26 20:10:01 -0700

    additions for consoleprinter methods
    added print backwards so string print backwards

--- ConsolePrinter.cs
+++ ConsolePrinter.cs
@@ -5,5 +5,13 @@ namespace ConsolePrinters {
         public static void print(string message) {
             System.Console.WriteLine(message);
         }
+
+        public static void printBackwards(string message) {
+            string backwordString = "";
+            for(int charIndex = message.Length - 1; charIndex >= 0; charIndex--) {
+                backwordString += message[charIndex];
+            }
+            System.Console.WriteLine(backwordString);
+        }
     }
 }
