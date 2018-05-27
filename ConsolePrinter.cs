096670bde472233621743bf950526e4299debe0d
NicDeSilva <nic.desilva99@gmail.com>
2018-05-26 20:07:39 -0700

    console printer file changes
    added print method

---
+++ ConsolePrinter.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace ConsolePrinters {
+    public static class Printers {
+        public static void print(string message) {
+            System.Console.WriteLine(message);
+        }
+    }
+}
